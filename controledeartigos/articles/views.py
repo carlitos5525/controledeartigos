a71593c9c02123fab9568c4557ed8e1f69d003d9
Carlos Mayer <carlitos5525.cap@gmail.com>
2021-05-30 20:20:49 -0300

    Add filter at articles_list

--- controledeartigos/articles/views.py
+++ controledeartigos/articles/views.py
@@ -5,14 +5,36 @@ from articles.forms import ArticleForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.http import Http404
+
 
 
 @login_required(login_url='/accounts/login')
 def flow_list_articles(request):
     template_name = 'articles/list_articles.html'
     articles = Article.objects.filter(is_deleted=False, product='FW').order_by('last_updated')
+    users = User.objects.all()
+    if request.GET.get('date_start_with') and request.GET.get('date_end_with'):
+            start_with = request.GET.get('date_start_with')
+            end_with = request.GET.get('date_end_with')
+            articles = articles.filter(last_updated__range=(start_with, end_with))
+    if request.GET.get('usuario'):
+        user_id = request.GET.get('usuario')
+        user = get_object_or_404(User, id=user_id)
+        articles = articles.filter(owner=user)    
+    if request.GET.get('query'):
+        query = request.GET.get('query')
+        articles = articles.filter(name__icontains=query)
+    if request.GET.get('pendente'):
+        articles = articles.filter(Q(status='AF') | Q(status='AR'))
+    if request.GET.get('realizado'):
+        articles = articles.filter(Q(status='FF') | Q(status='AO'))
+   
     context = {
-        'articles': articles
+        'articles': articles,
+        'product': 'Flow',
+        'users': users
     }
     return render(request, template_name, context)
 
@@ -20,8 +42,27 @@ def flow_list_articles(request):
 def start_list_articles(request):
     template_name = 'articles/list_articles.html'
     articles = Article.objects.filter(is_deleted=False, product='ST').order_by('last_updated')
+    users = User.objects.all()
+
+    if request.GET.get('date_start_with') and request.GET.get('date_end_with'):
+            start_with = request.GET.get('date_start_with')
+            end_with = request.GET.get('date_end_with')
+            articles = articles.filter(last_updated__range=(start_with, end_with))
+    if request.GET.get('usuario'):
+        user_id = request.GET.get('usuario')
+        user = get_object_or_404(User, id=user_id)
+        articles = articles.filter(owner=user)    
+    if request.GET.get('query'):
+        query = request.GET.get('query')
+        articles = articles.filter(name__icontains=query)
+    if request.GET.get('pendente'):
+        articles = articles.filter(Q(status='AF') | Q(status='AR'))
+    if request.GET.get('realizado'):
+        articles = articles.filter(Q(status='FF') | Q(status='AO'))
     context = {
-        'articles': articles
+        'articles': articles,
+        'product': 'Start',
+        'users': users
     }
     return render(request, template_name, context)
 
@@ -29,8 +70,26 @@ def start_list_articles(request):
 def celero_list_articles(request):
     template_name = 'articles/list_articles.html'
     articles = Article.objects.filter(is_deleted=False, product='CE').order_by('last_updated')
+    users = User.objects.all()
+    if request.GET.get('date_start_with') and request.GET.get('date_end_with'):
+            start_with = request.GET.get('date_start_with')
+            end_with = request.GET.get('date_end_with')
+            articles = articles.filter(last_updated__range=(start_with, end_with))
+    if request.GET.get('usuario'):
+        user_id = request.GET.get('usuario')
+        user = get_object_or_404(User, id=user_id)
+        articles = articles.filter(owner=user)    
+    if request.GET.get('query'):
+        query = request.GET.get('query')
+        articles = articles.filter(name__icontains=query)
+    if request.GET.get('pendente'):
+        articles = articles.filter(Q(status='AF') | Q(status='AR'))
+    if request.GET.get('realizado'):
+        articles = articles.filter(Q(status='FF') | Q(status='AO'))
     context = {
-        'articles': articles
+        'articles': articles,
+        'product': 'Celero',
+        'users': users
     }
     return render(request, template_name, context)
 
